816d14f733078f891bcd310449e142647615b9ac
rsbbc <pammalrsb@yahoo.com>
2017-03-14 12:52:56 -0500

    compiled loan

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -42,7 +42,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
-    err := stub.PutState("hello_world", []byte(args[0]))
+    err := stub.PutState("loan", []byte(args[0]))
     if err != nil {
         return nil, err
     }
